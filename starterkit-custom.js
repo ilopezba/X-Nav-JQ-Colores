1daa5b5ab495b64879a88365ace9dade1421eeab
Ivan Lopez Barrio <ivanlb01@gmail.com>
2015-02-25 19:58:57 +0100

    ejercicio hecho

---
+++ starterkit-custom.js
@@ -0,0 +1,11 @@
+
+jQuery(document).ready(function() {
+    $("#orderedlist").css("background-color","red");
+    $("#orderedlist li").addClass("blue");
+    $("#orderedlist > li:last").hover(function() {
+	        $(this).css("background-color","green");
+        },function(){
+	        $(this).css("background-color","red");
+        });
+});
+
